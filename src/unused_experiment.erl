0866d896709462854f4dc4cfbb3808371f50b1d6
Michael Taylor <michael@emte.net.au>
2022-09-21 23:33:13 +1000

    Add unused (ground) experiment
    Add experiment to detech fuses that ground unused IO pins.
    The `cpldfit` utility has an option to turn this on for all unused pins.

---
+++ src/unused_experiment.erl
@@ -0,0 +1,126 @@
+-module(unused_experiment).
+
+-export([run/0]).
+
+-export([internal/0]).
+
+%%====================================================================
+%% run
+%%====================================================================
+
+run() ->
+    {ok, _} = c:c(?MODULE),
+    ?MODULE:internal().
+
+%%--------------------------------------------------------------------
+
+internal() ->
+    lists:foreach(fun internal/1, density:list()),
+    ok.
+
+%%--------------------------------------------------------------------
+
+internal(Density) ->
+    io:format(" => unused ~s~n", [Density]),
+    Device = density:largest_device(Density),
+    MC_count = density:macro_cell_count(Density),
+    IOs0 = lists:sort([ IO || {_Pin, IO} <- device:io_pins(Device) ]),
+    IO_count = length(IOs0),
+    IOs = [lists:last(IOs0) | IOs0],
+    Experiments = experiments(Device, IOs, []),
+    {Floats, Grounds} = union(Experiments, [], []),
+    {Unused, []} = fuses:diff(Floats, Grounds),
+    MC_count = length(Unused),
+    Update = unused([lists:last(Experiments) | Experiments], Unused, []),
+    IO_count = length(Update),
+    fuses:update(Density, Update),
+    ok.
+
+%%--------------------------------------------------------------------
+
+experiments(_, [_], Experiments) ->
+    io:format("~n", []),
+    lists:reverse(Experiments);
+experiments(Device, [Output | IOs = [Input | _]], Experiments) ->
+    Float = experiment(Device, Output, Input, float),
+    Ground = experiment(Device, Output, Input, ground),
+    Experiment = {Input, Float, Ground},
+    experiments(Device, IOs, [Experiment | Experiments]).
+
+%%--------------------------------------------------------------------
+
+union([], Floats, Grounds) ->
+    {Floats, Grounds};
+union([{_, {_, Float}, {_, Ground}} | Experiments], Floats0, Grounds0) ->
+    Floats = fuses:union(Float, Floats0),
+    Grounds = fuses:union(Ground, Grounds0),
+    union(Experiments, Floats, Grounds).
+
+%%--------------------------------------------------------------------
+
+unused([_], _, Answers) ->
+    lists:reverse(Answers);
+unused([{MC, _, {_, G1}} | Es = [{_, _, {_, G2}} | _]], Unused, Answers) ->
+    Grounds = fuses:union(G1, G2),
+    [Fuse] = fuses:subtract(Unused, Grounds),
+    Name = list_to_atom(io_lib:format("~s_io_ground", [MC])),
+    Answer = {Name, Fuse},
+    unused(Es, Unused, [Answer | Answers]).
+
+%%--------------------------------------------------------------------
+
+%internal(Density) ->
+%    Device = density:largest_device(Density),
+%    IO = [
+%        fb01_01,
+%        fb01_02,
+%        fb01_04,
+%        fb02_01,
+%        fb02_04
+%    ],
+%    Experiments = [
+%        experiment(Device, Output, Input, Unused)
+%        ||
+%        Output <- IO,
+%        Input <- IO,
+%        Input =/= Output,
+%        Unused <- [float, ground]
+%    ],
+%    fuses:matrix(Experiments).
+
+%%====================================================================
+%% experiment
+%%====================================================================
+
+experiment(Device, Output, Input, Unused) ->
+    io:format(".", []),
+    %io:format(" => unused ~s ~s = ~s (~s)~n", [Device, Output, Input, Unused]),
+    Fuses = experiment:cache(#{
+        device => Device,
+        ucf => <<
+            "NET \"input\" LOC = \"", (macro_cell:name(Input))/binary, "\";\n"
+            "NET \"output\" LOC = \"", (macro_cell:name(Output))/binary, "\";\n"
+        >>,
+        unused => Unused,
+        vhdl => <<
+            "library IEEE;\n"
+            "use IEEE.STD_LOGIC_1164.ALL;\n"
+            "\n"
+            "entity experiment is\n"
+            "  port (\n"
+            "    input : in  STD_LOGIC;\n"
+            "    output : out STD_LOGIC\n"
+            "  );\n"
+            "end experiment;\n"
+            "\n"
+            "architecture behavioral of experiment is begin\n"
+            "  output <= input;\n"
+            "end behavioral;\n"
+        >>
+    }),
+    Name = iolist_to_binary(io_lib:format(
+        "~s <= ~s, ~s",
+        [Output, Input, Unused]
+    )),
+    {Name, Fuses}.
+
