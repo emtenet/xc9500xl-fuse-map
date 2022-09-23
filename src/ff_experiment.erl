5a2957996f7c3c6af7256d44b5dbff4a04f6e39b
Michael Taylor <michael@emte.net.au>
2022-09-23 23:41:42 +1000

    Detect toggle & init fuses
    The toggle fuse turns the default d-type flip-flop to a t-type.
    The init fuse sets the initial value of a flip-flop to '1' (default '0').

---
+++ src/ff_experiment.erl
@@ -0,0 +1,109 @@
+-module(ff_experiment).
+
+-export([run/0]).
+
+%%  Detect d/t-type flip-flop & initial fuse
+%%
+%%  Each macro-cell's flip-flop can be either d-type or t-type
+%%
+%%  Each macro-cell's flip-flop can be initialized to '0' or '1'
+
+%%====================================================================
+%% run
+%%====================================================================
+
+run() ->
+    [ run(Density) || Density <- density:list() ],
+    ok.
+
+%%--------------------------------------------------------------------
+
+run(Density) ->
+    Device = density:largest_device(Density),
+    MCs = device:io_macro_cells(Device),
+    GCKs = device:gck_macro_cells(Device),
+    Fuses = lists:flatten([
+        mc(Device, MC, MCs, gck(MC, GCKs))
+        ||
+        MC <- MCs
+    ]),
+    fuses:update(Density, Fuses).
+
+%%--------------------------------------------------------------------
+
+gck(MC, [MC, GCK | _]) ->
+    GCK;
+gck(_, [GCK | _]) ->
+    GCK.
+
+%%--------------------------------------------------------------------
+
+mc(Device, O, Avail0, GCK) ->
+    Used = [O, GCK],
+    {I, _} = unused(Avail0, Used),
+    Sources = [
+        source(d, I, GCK, O),
+        source(t, I, GCK, O),
+        source(init, I, GCK, O)
+    ],
+    Answers = [ experiment(Device, O, Source) || Source <- Sources ],
+    Matrix = fuses:matrix(Answers),
+    fuses:print(Matrix),
+    fuses(O, Matrix).
+
+%%--------------------------------------------------------------------
+
+unused([IO | IOs], Used) ->
+    case lists:member(IO, Used) of
+        true ->
+            unused(IOs, Used);
+
+        false ->
+            {IO, IOs}
+    end.
+
+%%--------------------------------------------------------------------
+
+source(Name, I, GCK, O) ->
+    FF = case Name of
+        d -> #{clk => gck, type => d};
+        t -> #{clk => gck, type => t};
+        init -> #{clk => gck, init => 1}
+    end,
+    {Name, [
+        {i, I},
+        {gck, GCK},
+        {o, O, i, FF}
+    ]}.
+
+%%--------------------------------------------------------------------
+
+experiment(Device, MC, {Name, Signals}) ->
+    {UCF, VHDL} = experiment:compile(Signals),
+    io:format(" => ff ~s ~s ~s~n", [Device, MC, Name]),
+    {Name, experiment:cache(#{
+        device => Device,
+        usercode => <<"@@@@">>,
+        ucf => UCF,
+        vhdl => VHDL
+    })}.
+
+%%--------------------------------------------------------------------
+
+
+fuses(MC, {matrix,
+           [T, INIT],
+           [{d,    [off, off]},
+            {t,    [on,  off]},
+            {init, [off, on ]}
+           ]
+          }) ->
+    [fuse(T, MC, ff_toggle),
+     fuse(INIT, MC, ff_init)
+    ].
+
+%%--------------------------------------------------------------------
+
+fuse(Fuse, MC, Extra) ->
+    {Fuse, list_to_atom(io_lib:format("~s_~s", [MC, Extra]))}.
+
