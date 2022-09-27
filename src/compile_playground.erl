cc84beff05cb4805351e5fd047004fe929670708
Michael Taylor <michael@emte.net.au>
2022-09-27 13:02:24 +1000

    Refresh playgrounds, try an internal flip-flop

---
+++ src/compile_playground.erl
@@ -0,0 +1,38 @@
+-module(compile_playground).
+
+-export([run/0]).
+
+%%====================================================================
+%% run
+%%====================================================================
+
+run() ->
+    Density = xc9536xl,
+    io:format(" => compile playground ~s~n", [Density]),
+    Device = density:largest_device(Density),
+    experiment(Device),
+    Fuses = experiment:jed(),
+    IMUX = experiment:imux(),
+    Database = fuses:read(Density),
+    Names = fuses:name_if_known(Fuses, Database),
+    io:format("~p~n", [Names]),
+    io:format("~p~n", [IMUX]).
+
+%%====================================================================
+%% experiment
+%%====================================================================
+
+experiment(Device) ->
+    {UCF, VHDL} = experiment:compile([
+        {input, mc01_02},
+        {clock, mc01_03, #{global => gck}},
+        {hidden, mc01_02, input, internal, #{clk => clock}},
+        {output, mc01_01, hidden, #{clk => clock}}
+    ]),
+    experiment:run(#{
+        device => Device,
+        slew => slow,
+        ucf => UCF,
+        vhdl => VHDL
+    }).
+
---
+++ src/vhdl_playground.erl
@@ -0,0 +1,65 @@
+-module(vhdl_playground).
+
+-export([run/0]).
+
+%%====================================================================
+%% run
+%%====================================================================
+
+run() ->
+    Density = xc9536xl,
+    io:format(" => VHDL playground ~s~n", [Density]),
+    Device = density:largest_device(Density),
+    experiment(Device),
+    Fuses = experiment:jed(),
+    IMUX = experiment:imux(),
+    Database = fuses:read(Density),
+    Names = fuses:name_if_known(Fuses, Database),
+    io:format("~p~n", [Names]),
+    io:format("~p~n", [IMUX]).
+
+%%====================================================================
+%% experiment
+%%====================================================================
+
+experiment(Device) ->
+    experiment:run(#{
+        device => Device,
+        slew => slow,
+        ucf => <<
+            "NET \"input\" LOC = \"FB1_2\";\n"
+            "NET \"clock\" LOC = \"FB1_3\" | BUFG = CLK;\n"
+            "NET \"output\" LOC = \"FB1_1\";\n"
+            "NET \"hidden\" LOC = \"FB1_2\";\n"
+        >>,
+        vhdl => <<
+            "library IEEE;\n"
+            "use IEEE.STD_LOGIC_1164.ALL;\n"
+            "library UNISIM;\n"
+            "use UNISIM.vcomponents.ALL;\n"
+            "\n"
+            "entity experiment is\n"
+            "  port (\n"
+            "    input : in STD_LOGIC;\n"
+            "    clock : in STD_LOGIC;\n"
+            "    output : out STD_LOGIC\n"
+            "  );\n"
+            "end experiment;\n"
+            "\n"
+            "architecture behavioral of experiment is\n"
+            "  signal hidden : STD_LOGIC;\n"
+            "begin\n"
+            "  hidden_FF: FD generic map ('0') port map (\n"
+            "    D => NOT input,\n"
+            "    Q => hidden,\n"
+            "    C => clock\n"
+            "  );\n"
+            "  output_FF: FD generic map ('0') port map (\n"
+            "    D => hidden,\n"
+            "    Q => output,\n"
+            "    C => clock\n"
+            "  );\n"
+            "end behavioral;\n"
+        >>
+    }).
+
