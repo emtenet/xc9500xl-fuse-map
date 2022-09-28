244f2d028eb74fa3344e9a7deb768ae273d1fa1d
Michael Taylor <michael@emte.net.au>
2022-09-28 20:46:18 +1000

    Define type fuse:user()

--- src/fuse.erl
+++ src/fuse.erl
@@ -5,6 +5,13 @@
 -export([fast_connect/4]).
 -export([macro_cell/2]).
 
+-type user() ::
+    user00 | user01 | user02 | user03 | user04 | user05 | user06 | user07 |
+    user08 | user09 | user10 | user11 | user12 | user13 | user14 | user15 |
+    user16 | user17 | user18 | user19 | user20 | user21 | user22 | user23 |
+    user24 | user25 | user26 | user27 | user28 | user29 | user30 | user13.
+-export_type([user/0]).
+
 %%====================================================================
 %% and_array
 %%====================================================================
